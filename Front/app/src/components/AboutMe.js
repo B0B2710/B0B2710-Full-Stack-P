1f5cfe3c58fc186e8c4923bc320ac4d9edf507ab
BOB <93728583+B0B2710@users.noreply.github.com>
2024-02-08 18:06:53 +0200

    updated a bit the about me (not finnished)

--- Front/app/src/components/AboutMe.js
+++ Front/app/src/components/AboutMe.js
@@ -1,11 +1,41 @@
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
+import { motion, px, useAnimation } from 'framer-motion';
+import { IoLogoJavascript } from "react-icons/io5"; //javascript icon
+import { FaPython } from "react-icons/fa";//python icon
+import { FaDiscord } from "react-icons/fa";//discord icon
+import { DiVisualstudio } from "react-icons/di";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNodeJs } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { MdCss } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+
 import '../styles/AboutMe.css';
+
+
 
 const AboutMe = () => {
   const controls = useAnimation();
 
+ const [counter,setCounter] = useState(0)
 
+ const counterplus = () => {
+  console.log(`lengh ${boxes.length} counter : ${counter}`)
+  if(counter === boxes.length - 1){
+    return;
+  }
+  setCounter(counter+1)
+
+};
+const counterminus = () => {
+  if(counter===0){
+    return;
+  }
+  setCounter(counter-1)
+  console.log(counter)
+};
   useEffect(() => {
     controls.start({
       opacity: 1,
@@ -22,21 +52,54 @@ const AboutMe = () => {
   };
 
   return (
+    <>
     <motion.div className="about-me-container" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <motion.div animate={controls} className="about-me-card">
         <h1 className="about-me-title">Hey, I'm Rodion!</h1>
-        <motion.p {...textAnimation} className="about-me-text">
-          Welcome to my world of coding adventures! At the age of 16, I've embarked on an exciting journey through the realms of Python, JavaScript, C#, Java, HTML, and CSS – weaving digital wonders with every keystroke.
-        </motion.p>
-        <motion.p {...textAnimation} className="about-me-text">
-          As a coding enthusiast, I navigate the vast landscape of React and Next.js, crafting dynamic and responsive user interfaces. But that's not all! I've also harnessed the power of the Discord API to build interactive and engaging chat experiences.
-        </motion.p>
-        <motion.p {...textAnimation} className="about-me-text">
-          Beyond the screen, you'll find me in a realm where creativity meets logic, where bugs are mysterious creatures to be tamed, and where each project is an opportunity for growth. Join me as I continue to unravel the secrets of the coding universe!
-        </motion.p>
+        <div>
+          <h2>Im a self learned full-stack web Developer and Discord Bots Developer</h2>
+        </div>
       </motion.div>
     </motion.div>
+    <motion.div className='tools-and-langs-box-container'>
+      <h3 className='tools-and-langs-box-title'>Here are the Programing langs i know and still learns</h3>
+
+    <motion.div className='Buttons'>
+      <button onClick={counterminus} className='Button'>before</button>
+      <button onClick={counterplus} className='Button'>next</button>
+    </motion.div>
+      <motion.div className='langs-box-container'>
+      <motion.div className='langs-box'>
+        <h2>{boxes[counter].title}</h2>
+        <div className='img'>{boxes[counter].img}</div>
+        
+        <h3>{boxes[counter].text}</h3>
+        
+        </motion.div>
+      </motion.div>
+      <button></button>
+      <h2>And here some of the tools i use to do my Coding :D</h2>
+      <motion.div className='tools-box-container'> 
+      
+      </motion.div>
+    </motion.div>
+
+    </>
+    
   );
 };
 
+
+const boxes = [
+  {
+    title:"js",
+    img:<IoLogoJavascript />,
+    text:"im useing js to build this app rn "
+  },
+  {
+    title:"pytohn",
+    img:<FaPython/>,
+    text:"i have no idea how to use python :( "
+  }
+  ]
 export default AboutMe;
